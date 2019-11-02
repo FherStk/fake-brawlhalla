6f9dee24e9d406cd70de0cf3864af9e9aaf9608c
daniellozinak <danoloznak@gmail.com>
2019-11-02 14:20:33 +0100

    test

--- src/Fakehalla/Main.java
+++ src/Fakehalla/Main.java
@@ -1,14 +1,10 @@
 package Fakehalla;
-
+/* lets see if this works*/
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
 
 public class Main extends Application {
     public static void main (String[] args)
